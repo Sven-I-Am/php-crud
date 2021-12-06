3e36520b93c28cb88b2c8b66e8fee503faaf31a3
Sven-I-Am <sven.vander.mierde@gmail.com>
2021-12-06 11:02:24 +0100

    fixed order of table creation in import.sql

--- Resources/import.sql
+++ Resources/import.sql
@@ -1,12 +1,3 @@
-DROP TABLE IF EXISTS Student;
-CREATE TABLE Student (
-   Name varchar(255) NOT NULL,
-   Email varchar(255) NOT NULL,
-   Class varchar(255),
-   PRIMARY KEY (Name),
-   FOREIGN KEY (Class) REFERENCES Class(Name)
-);
-
 DROP TABLE IF EXISTS Teacher;
 CREATE TABLE Teacher (
      Name varchar(255) NOT NULL,
@@ -21,4 +12,13 @@ CREATE TABLE Class (
      Teacher varchar(255),
      PRIMARY KEY (Name),
      FOREIGN KEY (Teacher) REFERENCES Teacher(Name)
+);
+
+DROP TABLE IF EXISTS Student;
+CREATE TABLE Student (
+     Name varchar(255) NOT NULL,
+     Email varchar(255) NOT NULL,
+     Class varchar(255),
+     PRIMARY KEY (Name),
+     FOREIGN KEY (Class) REFERENCES Class(Name)
 );
