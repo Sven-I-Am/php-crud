87e10f45795a3c232a7c3342da52f1bb64729ed3
830503 <lixiaoqi19830503@gmail.com>
2021-12-06 12:21:38 +0100

    merge

--- Resources/import.sql
+++ Resources/import.sql
@@ -1,18 +1,15 @@
-DROP TABLE IF EXISTS Student;
-CREATE TABLE Student (
-   Name varchar(255) NOT NULL,
-   Email varchar(255) NOT NULL,
-   Class varchar(255),
-   PRIMARY KEY (Name),
-   FOREIGN KEY (Class) REFERENCES Class(Name)
-);
-
 DROP TABLE IF EXISTS Teacher;
 CREATE TABLE Teacher (
      Name varchar(255) NOT NULL,
      Email varchar(255),
      PRIMARY KEY (Name)
 );
+
+INSERT INTO Teacher(Name, Email) VALUES ('Tim Broos', 'tim.broos@becode.org');
+INSERT INTO Teacher(Name, Email) VALUES ('Ludovic Patho', 'ludovic.patho@becode.org');
+INSERT INTO Teacher(Name, Email) VALUES ('Arnaud Duchemin', 'arnaud.uchemin@becode.org');
+INSERT INTO Teacher(Name, Email) VALUES ('Nicolas Jamar', 'nicolas.jamar@becode.org');
+
 
 DROP TABLE IF EXISTS Class;
 CREATE TABLE Class (
@@ -22,3 +19,13 @@ CREATE TABLE Class (
      PRIMARY KEY (Name),
      FOREIGN KEY (Teacher) REFERENCES Teacher(Name)
 );
+
+DROP TABLE IF EXISTS Student;
+CREATE TABLE Student (
+   Name varchar(255) NOT NULL,
+   Email varchar(255) NOT NULL,
+   Class varchar(255),
+   PRIMARY KEY (Name),
+   FOREIGN KEY (Class) REFERENCES Class(Name)
+);
+
