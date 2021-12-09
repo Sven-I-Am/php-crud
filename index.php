a6b570cc57b3cdbd67a3b8f2bc1068ed4b66b4af
Sven-I-Am <sven.vander.mierde@gmail.com>
2021-12-09 07:37:02 +0100

    fixed error when $_GET=empty

--- index.php
+++ index.php
@@ -4,19 +4,7 @@ declare(strict_types=1);
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
-//NEVER FORGET!!!! CANNOT HAVE SESSION VARIABLES WITHOUT SESSIONS TART!!
-session_start();
 
-function whatIsHappening() {
-    echo '<h2>$_GET</h2>';
-    var_dump($_GET);
-    echo '<h2>$_POST</h2>';
-    var_dump($_POST);
-    echo '<h2>$_COOKIE</h2>';
-    var_dump($_COOKIE);
-    echo '<h2>$_SESSION</h2>';
-    var_dump($_SESSION);
-}
 //include all your model files here
 
 require 'Model/connection.php';
@@ -44,15 +32,23 @@ $classController = new classController();
 
 if(empty($_GET) || $_GET["page"] === "home"){
     $homeController->render($_GET, $_POST);
-}
-if($_GET["page"] === "students" || $_GET["page"] === "addStudent" || $_GET["page"] === "studentAdd" || $_GET["page"] === "viewStudent" || $_GET["page"] === "editStudent" || $_GET["page"] === "studentEdit" || $_GET["page"] === "deleteStudent"){
+}else if($_GET["page"] === "students" || $_GET["page"] === "addStudent" || $_GET["page"] === "studentAdd" || $_GET["page"] === "viewStudent" || $_GET["page"] === "editStudent" || $_GET["page"] === "studentEdit" || $_GET["page"] === "deleteStudent"){
     $studentController->render($_GET, $_POST);
-}
-if(empty($_GET) || $_GET["page"] === "teachers" || $_GET["page"] === "viewTeacher" || $_GET["page"] === "addTeacher" || $_GET["page"] === "teacherAdd" || $_GET["page"] === "editTeacher" || $_GET["page"] === "teacherEdit" || $_GET["page"] === "deleteTeacher") {
+}else if(empty($_GET) || $_GET["page"] === "teachers" || $_GET["page"] === "viewTeacher" || $_GET["page"] === "addTeacher" || $_GET["page"] === "teacherAdd" || $_GET["page"] === "editTeacher" || $_GET["page"] === "teacherEdit" || $_GET["page"] === "deleteTeacher") {
     $teacherController->render($_GET, $_POST);
-}
-if($_GET["page"] === "classes" || $_GET["page"] === "addClass" || $_GET["page"] === "classAdd" || $_GET["page"] === "viewClass" || $_GET["page"] === "editClass" || $_GET["page"] === "classEdit" || $_GET["page"] === "deleteClass"){
+} else if($_GET["page"] === "classes" || $_GET["page"] === "addClass" || $_GET["page"] === "classAdd" || $_GET["page"] === "viewClass" || $_GET["page"] === "editClass" || $_GET["page"] === "classEdit" || $_GET["page"] === "deleteClass"){
     $classController->render($_GET, $_POST);
 }
 
 //whatIsHappening();
+
+function whatIsHappening() {
+    echo '<h2>$_GET</h2>';
+    var_dump($_GET);
+    echo '<h2>$_POST</h2>';
+    var_dump($_POST);
+    echo '<h2>$_COOKIE</h2>';
+    var_dump($_COOKIE);
+    echo '<h2>$_SESSION</h2>';
+    var_dump($_SESSION);
+}
